1ef46aaeebf02d2f5b48574c6c482a72b8a1a56b
Tyler K <tylerkruer1@gmail.com>
2022-10-04 08:28:31 -0700

    init commit

---
+++ Polyglot-Swift/Polyglot-Swift/Polyglot_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Polyglot_SwiftApp.swift
+//  Polyglot-Swift
+//
+//  Created by Tyler Kruer on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Polyglot_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
